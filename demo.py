4e49fa2d40aea37e4c209320ba622b20f0fadd95
sauravdevp <saurav.kumar@brightcodess.com>
2022-11-08 13:24:56 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+https://www.codewithharry.com/blogpost/no-module-named-mysql-error/
+
+#// to connect django with mysql db link above follow to install and connect 
+
+https://www.lfd.uci.edu/~gohlke/pythonlibs/#mysqlclient
+
+#// donwload file msyqclient to connect link above
---
+++ mydb/demo.py
@@ -0,0 +1,9 @@
+https://www.codewithharry.com/blogpost/no-module-named-mysql-error/
+
+#// to connect django with mysql db link above follow to install and connect 
+
+https://www.lfd.uci.edu/~gohlke/pythonlibs/#mysqlclient
+
+#// donwload file msyqclient to connect link above
+
+# file is also download and save in mydb project folder parallel to manage.py for python 3.8 and windows 32 bit
